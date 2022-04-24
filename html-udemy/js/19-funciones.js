fb622841b4583cf5481a364cebd79b0f7b56345d
aretoso1 <lghb04@gmail.com>
2022-04-23 23:24:29 -0400

    calculadora con parametros opcionales

--- html-udemy/js/19-funciones.js
+++ html-udemy/js/19-funciones.js
@@ -2,16 +2,32 @@
 
 //Funciones
 
-function calculadora(numero1,numero2){
-    console.log("Suma "+(numero1+numero2));
-    console.log("Resta "+(numero1-numero2));
-    console.log("Multiplicación "+(numero1*numero2));
-    console.log("División "+(numero1/numero2));
-    console.log("******************************");
-    //console.log("Hola soy la calculadora");
-    //console.log("Soy memito");
-    return "Hola soy la calculadora";
+function calculadora(numero1,numero2, mostrar=false){
+    
+    if(mostrar==false){
+        console.log("Suma "+(numero1+numero2));
+        console.log("Resta "+(numero1-numero2));
+        console.log("Multiplicación "+(numero1*numero2));
+        console.log("División "+(numero1/numero2));
+        console.log("******************************");
+        //console.log("Hola soy la calculadora");
+        //console.log("Soy memito");
+        return "Hola soy la calculadora";
+    }else{
+        document.write("Suma "+(numero1+numero2)+"</br>");
+        document.write("Resta "+(numero1-numero2)+"</br>");
+        document.write("Multiplicación "+(numero1*numero2)+"</br>");
+        document.write("División "+(numero1/numero2)+"</br>");
+        document.write("******************************"+"</br>");
+        //console.log("Hola soy la calculadora");
+        //console.log("Soy memito");
+        //return "Hola soy la calculadora";
+    }
+    
+    
 }
+
+//calculadora(13,17,true);
 
 //console.log(calculadora(),calculadora())
 //calculadora();
@@ -19,6 +35,11 @@ function calculadora(numero1,numero2){
 
 //var resultado=calculadora(2,5);
 //console.log(resultado);
+
+for(var i=1; i<=10;i++){
+    console.log(i);
+    calculadora(i,17,true);
+}
 
 for(var i=1; i<=10;i++){
     console.log(i);
