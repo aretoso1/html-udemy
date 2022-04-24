70d8162447f86619f90a25670166cf324bdc0533
aretoso1 <lghb04@gmail.com>
2022-04-23 22:27:32 -0400

    return con variable

--- html-udemy/js/19-funciones.js
+++ html-udemy/js/19-funciones.js
@@ -5,9 +5,12 @@
 function calculadora(){
     console.log("Hola soy la calculadora");
     console.log("Soy memito");
-    //return "Hola soy la calculadora";
+    return "Hola soy la calculadora";
 }
 
 //console.log(calculadora(),calculadora())
-calculadora();
-calculadora();
+//calculadora();
+//calculadora();
+
+var resultado=calculadora();
+console.log(resultado);
